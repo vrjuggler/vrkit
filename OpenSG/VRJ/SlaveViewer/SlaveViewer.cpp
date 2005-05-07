01eccce7ad49ee2d2598926abd0be39b0eafb107
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2005-05-07 13:58:20 +0000

    Initial version of the "dummy" OpenSG-based VR Juggler application that simply displays a scene graph shared by OpenSG.  This turned out to be very easy to write, and it even worked on the first try.  However, it still has at least one or two kinks to work out, and there is no lighting defined.  At this point, this has only been tested on Mac OS X with the good old RemoteViewer application written last year as part of the MaxVR work.

---
+++ OpenSG/VRJ/SlaveViewer/SlaveViewer.cpp
@@ -0,0 +1,234 @@
+#include <iostream>
+#include <stdlib.h>
+
+#include <OpenSG/OSGAttachmentContainer.h>
+#include <OpenSG/OSGConnectionFactory.h>
+#include <OpenSG/OSGFieldContainerFactory.h>
+#include <OpenSG/OSGNode.h>
+#include <OpenSG/OSGThread.h>
+#include <OpenSG/OSGTransform.h>
+#include <OpenSG/OSGGeometry.h>
+#include <OpenSG/OSGSimpleAttachments.h>
+
+#include <OpenSG/VRJ/SlaveViewer/SlaveViewer.h>
+
+
+namespace
+{
+
+std::vector<OSG::AttachmentContainerPtr> gMaybeNamedFcs;
+unsigned int gNodes(0);
+unsigned int gTransforms(0);
+unsigned int gGeometries(0);
+
+bool changedFunction(OSG::FieldContainerPtr& fcp, OSG::RemoteAspect*)
+{
+   std::cout << "Changed: " << fcp->getType().getName().str() << " "
+             << fcp.getFieldContainerId() << std::endl;
+
+   OSG::AttachmentContainerPtr acp = OSG::AttachmentContainerPtr::dcast(fcp);
+
+   if ( OSG::NullFC != acp )
+   {
+      const char* node_name = OSG::getName(acp);
+      if ( NULL == node_name )
+      {
+         std::cout << "<NULL>" << std::endl;
+      }
+      else
+      {
+         std::cout << "\tname: " << node_name << std::endl;
+      }
+   }
+
+   return true;
+}
+
+bool createdFunction(OSG::FieldContainerPtr& fcp, OSG::RemoteAspect*)
+{
+   if ( OSG::Node::getClassType() == fcp->getType() )
+   {
+      ++gNodes;
+   }
+   else if ( OSG::Transform::getClassType() == fcp->getType() )
+   {
+      ++gTransforms;
+   }
+   else if ( OSG::Geometry::getClassType() == fcp->getType() )
+   {
+      ++gGeometries;
+   }
+
+   std::cout << "Created: " << fcp->getType().getName().str() << " "
+             << fcp.getFieldContainerId() << std::endl;
+
+   OSG::AttachmentContainerPtr acp = OSG::AttachmentContainerPtr::dcast(fcp);
+
+   if ( OSG::NullFC != acp )
+   {
+      const char* node_name = OSG::getName(acp);
+
+      if ( NULL == node_name )
+      {
+         std::cout << "<NULL>" << std::endl;
+      }
+      else
+      {
+         std::cout << "\tname: " << node_name << std::endl;
+      }
+
+      gMaybeNamedFcs.push_back(acp);
+   }
+
+   return true;
+}
+
+bool destroyedFunction(OSG::FieldContainerPtr& fcp, OSG::RemoteAspect*)
+{
+   if ( OSG::Node::getClassType() == fcp->getType() )
+   {
+      --gNodes;
+   }
+   else if ( OSG::Transform::getClassType() == fcp->getType() )
+   {
+      --gTransforms;
+   }
+   else if ( OSG::Geometry::getClassType() == fcp->getType() )
+   {
+      --gGeometries;
+   }
+
+   return true;
+}
+
+}
+
+namespace inf
+{
+
+SlaveViewer::SlaveViewer(const std::string& masterAddr,
+                         const std::string& rootNodeName)
+   : vrj::OpenSGApp()
+   , mMasterAddr(masterAddr)
+   , mRootNodeName(rootNodeName)
+   , mAspect(new OSG::RemoteAspect())
+   , mConnection(NULL)
+{
+   mConnection = OSG::ConnectionFactory::the().createPoint("StreamSock");
+}
+
+SlaveViewer::~SlaveViewer()
+{
+   if ( NULL != mConnection )
+   {
+      delete mConnection;
+   }
+
+   if ( NULL != mAspect )
+   {
+      delete mAspect;
+   }
+}
+
+void SlaveViewer::initScene()
+{
+   OSG::RemoteAspect::Functor changed =
+      OSG::osgTypedFunctionFunctor2CPtrRef<bool, OSG::FieldContainerPtr, OSG::RemoteAspect*>(changedFunction);
+   OSG::RemoteAspect::Functor destroyed =
+      OSG::osgTypedFunctionFunctor2CPtrRef<bool, OSG::FieldContainerPtr, OSG::RemoteAspect*>(destroyedFunction);
+   OSG::RemoteAspect::Functor created =
+      OSG::osgTypedFunctionFunctor2CPtrRef<bool, OSG::FieldContainerPtr, OSG::RemoteAspect*>(createdFunction);
+
+   for ( OSG::UInt32 i = 0; i < OSG::TypeFactory::the()->getNumTypes(); ++i )
+   {
+      OSG::FieldContainerType* fct =
+         OSG::FieldContainerFactory::the()->findType(i);
+      if ( NULL != fct )
+      {
+         mAspect->registerChanged(*fct, changed);
+         mAspect->registerDestroyed(*fct, destroyed);
+         mAspect->registerCreated(*fct, created);
+      }
+   }
+
+   mConnection->connectPoint(mMasterAddr);
+   mConnection->selectChannel();
+
+   mConnection->wait();
+   mAspect->receiveSync(*mConnection);
+
+   OSG::Thread::getCurrentChangeList()->clearAll();
+   int finish(0);
+   mConnection->getValue(finish);
+
+   std::cout << "--- Field Containers ---" << std::endl;
+   for ( unsigned int i = 0; i < gMaybeNamedFcs.size(); ++i )
+   {
+      OSG::AttachmentContainerPtr acp = gMaybeNamedFcs[i];
+
+      const char* node_name = OSG::getName(acp);
+
+      if ( NULL == node_name )
+      {
+         std::cout << i << ": <NULL>" << std::endl;
+      }
+      else
+      {
+         std::cout << i << ": " << node_name << std::endl;
+      }
+   }
+   std::cout << "------" << std::endl;
+
+   std::cout << "--- Searching for scene root (name is " << mRootNodeName
+             << ") ---" << std::endl;
+
+   const std::vector<OSG::FieldContainerPtr>* fcs =
+      OSG::FieldContainerFactory::the()->getFieldContainerStore();
+
+   for ( OSG::UInt32 i = 0; i < fcs->size(); ++i )
+   {
+      OSG::NodePtr node = OSG::NodePtr::dcast((*fcs)[i]);
+
+      if ( OSG::NullFC != node )
+      {
+         const char* node_name = OSG::getName(node);
+
+         if ( node_name != NULL && std::string(node_name) == mRootNodeName )
+         {
+            std::cout << "Found it." << std::endl;
+            mSceneRoot = node;
+            break;
+         }
+      }
+   }
+
+   if ( OSG::NullFC == mSceneRoot.node() )
+   {
+      std::cout << "NOT FOUND." << std::endl;
+      ::exit(-256);
+   }
+
+   std::cout << "------" << std::endl;
+}
+
+void SlaveViewer::preFrame()
+{
+   int finish(0);
+
+   if ( mConnection->wait(0) )
+   {
+      mAspect->receiveSync(*mConnection);
+      OSG::Thread::getCurrentChangeList()->clearAll();
+      mConnection->getValue(finish);
+   }
+
+   if ( finish != 0 )
+   {
+      mConnection->putValue(finish);
+      mConnection->flush();
+
+      OSG::osgExit();
+   }
+}
+
+}
---
+++ OpenSG/VRJ/SlaveViewer/SlaveViewer.h
@@ -0,0 +1,49 @@
+#ifndef _INF_SLAVE_VIEWER_H_
+#define _INF_SLAVE_VIEWER_H_
+
+#include <string>
+
+#include <OpenSG/OSGGroup.h>
+#include <OpenSG/OSGNode.h>
+#include <OpenSG/OSGCoredNodePtr.h>
+#include <OpenSG/OSGRemoteAspect.h>
+#include <OpenSG/OSGPointConnection.h>
+
+#include <vrj/Draw/OpenSG/OpenSGApp.h>
+
+
+namespace inf
+{
+
+typedef OSG::CoredNodePtr<OSG::Group> CoredGroupPtr;
+
+class SlaveViewer : public vrj::OpenSGApp
+{
+public:
+   SlaveViewer(const std::string& masterAddr,
+               const std::string& rootNodeName = "RootNode");
+
+   virtual ~SlaveViewer();
+
+   virtual void initScene();
+
+   virtual OSG::NodePtr getScene()
+   {
+      return mSceneRoot.node();
+   }
+
+   virtual void preFrame();
+
+private:
+   std::string mMasterAddr;
+   std::string mRootNodeName;
+
+   CoredGroupPtr mSceneRoot;
+
+   OSG::RemoteAspect*    mAspect;
+   OSG::PointConnection* mConnection;
+};
+
+}
+
+#endif
