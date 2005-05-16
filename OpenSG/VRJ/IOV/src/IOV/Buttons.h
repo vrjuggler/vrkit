d9a9495de6d00306183d9c4d41f2127c76fb4c94
allenb <allenb@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2005-05-16 20:06:11 +0000

    Create plugin directory. Move definitions.  create a viewer directory for the test application.

---
+++ OpenSG/VRJ/IOV/src/IOV/Buttons.h
@@ -0,0 +1,22 @@
+#ifndef _INF_PLUGIN_BUTTONS_H_
+#define _INF_PLUGIN_BUTTONS_H_
+
+
+namespace inf
+{
+
+namespace buttons
+{
+
+const unsigned int GRAB_TOGGLE_BUTTON(0);
+const unsigned int NAV_GO_BUTTON(0);
+const unsigned int NAV_ROTATE_BUTTON(1);
+const unsigned int NAV_TOGGLE_BUTTON(2);
+const unsigned int MODE_SWITCH_BUTTON(3);
+
+}
+
+}
+
+
+#endif
