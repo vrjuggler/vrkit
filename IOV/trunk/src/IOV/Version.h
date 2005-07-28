fb0bdaa80f6284ef7c4c13c3c7aef14785e79094
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2005-07-28 15:23:08 +0000

    Note the clustering bug fix I just made in my favorite class, inf::GeometryHighlightTraverser.

--- IOV/trunk/src/IOV/Version.h
+++ IOV/trunk/src/IOV/Version.h
@@ -19,7 +19,7 @@
 // The major/minor/patch version (up to 3 digits each).
 #define IOV_VERSION_MAJOR    0
 #define IOV_VERSION_MINOR    8
-#define IOV_VERSION_PATCH    0
+#define IOV_VERSION_PATCH    1
 
 //--------------------------------------------------------------------------
 //--------------------------------------------------------------------------
