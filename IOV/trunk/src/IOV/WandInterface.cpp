6a6d4c3558f8892806a25aa16721bc459e4af5fc
allenb <allenb@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2005-08-23 19:24:25 +0000

    digital buttons.

--- IOV/trunk/src/IOV/WandInterface.cpp
+++ IOV/trunk/src/IOV/WandInterface.cpp
@@ -25,12 +25,13 @@ void WandInterface::init()
 {
    mWandInterface.init("VJWand");
 
-   mButtonInterfaces.resize(5);
+   mButtonInterfaces.resize(6);
    mButtonInterfaces[0].init("VJButton0");
    mButtonInterfaces[1].init("VJButton1");
    mButtonInterfaces[2].init("VJButton2");
    mButtonInterfaces[3].init("VJButton3");
    mButtonInterfaces[4].init("VJButton4");
+   mButtonInterfaces[5].init("VJButton5");
 }
 
 }
