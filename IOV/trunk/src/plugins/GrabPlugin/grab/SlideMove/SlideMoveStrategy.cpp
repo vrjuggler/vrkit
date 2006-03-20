fccac3ec4f7dc709d52e58b681ba89774e542063
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2006-03-20 15:59:08 +0000

    Do not perform the sliding operations if the analog device proxy is stupefied. In that case, the device will always return 0. Since this plug-in expects the at-rest value for the analog device to be 0.5, a stupefied proxy will cause the grabbed object to slide uncontrollably.

--- IOV/trunk/src/plugins/GrabPlugin/grab/SlideMove/SlideMoveStrategy.cpp
+++ IOV/trunk/src/plugins/GrabPlugin/grab/SlideMove/SlideMoveStrategy.cpp
@@ -68,69 +68,75 @@ SlideMoveStrategy::computeMove(inf::ViewerPtr viewer,
 {
    int mInOutAnalogNum = 0;
 
-   WandInterfacePtr wand_if = viewer->getUser()->getInterfaceTrader().getWandInterface();
+   WandInterfacePtr wand_if =
+      viewer->getUser()->getInterfaceTrader().getWandInterface();
+   gadget::AnalogInterface& analog_dev = wand_if->getAnalog(mInOutAnalogNum);
 
-   float analog_value = wand_if->getAnalog(mInOutAnalogNum)->getData();
-   //std::cout << "Analog Value: " << analog_value << std::endl;
-
-   // Rescale [0,1] to [-1,1]
-   float in_out_val = (analog_value*2.0)-1.0f;
-
-   //std::cout << "Analog values: " << analog0_val << ", " << analog1_val << std::endl;
-   const float eps_limit(0.1);
-
-   if(fabs(in_out_val) < eps_limit)
-   { in_out_val = 0.0f; }
-
-   // If we don't have an analog configured, then set in-out to 0
-   if(-1 == mInOutAnalogNum)
-   { in_out_val = 0.0f; }
-
-   //std::cout << "In out: " << in_out_val << std::endl;
-
-   gmtl::Matrix44f new_obj_pos;
-   const float in_out_scale = 0.20f;
-   float trans_val = -in_out_val*in_out_scale;
-   mTransValue += trans_val;
-
-   // If the object is at the wand then don't allow it to
-   // move any further back.
-   if (mTransValue >= 1.0)
+   if ( ! analog_dev->isStupefied() )
    {
-      mTransValue = 1.0;
+      float analog_value = wand_if->getAnalog(mInOutAnalogNum)->getData();
+      //std::cout << "Analog Value: " << analog_value << std::endl;
+
+      // Rescale [0,1] to [-1,1]
+      float in_out_val = (analog_value*2.0)-1.0f;
+
+      //std::cout << "Analog values: " << analog0_val << ", " << analog1_val
+      //          << std::endl;
+      const float eps_limit(0.1);
+
+      if(fabs(in_out_val) < eps_limit)
+      { in_out_val = 0.0f; }
+
+      // If we don't have an analog configured, then set in-out to 0
+      if(-1 == mInOutAnalogNum)
+      { in_out_val = 0.0f; }
+
+      //std::cout << "In out: " << in_out_val << std::endl;
+
+      gmtl::Matrix44f new_obj_pos;
+      const float in_out_scale = 0.20f;
+      float trans_val = -in_out_val*in_out_scale;
+      mTransValue += trans_val;
+
+      // If the object is at the wand then don't allow it to
+      // move any further back.
+      if (mTransValue >= 1.0)
+      {
+         mTransValue = 1.0;
+      }
+
+      // pobj_M_vp is the inverse of the object in view platform space.
+      OSG::Matrix world_xform;
+      obj.node()->getParent()->getToWorld(world_xform);
+      gmtl::Matrix44f pobj_M_vp;
+      gmtl::set(pobj_M_vp, world_xform);
+      gmtl::invert(pobj_M_vp);
+
+      const gmtl::Matrix44f pobj_M_wand = pobj_M_vp * vp_M_wand;
+      gmtl::Matrix44f wand_M_pobj;
+      gmtl::invert(wand_M_pobj, pobj_M_wand);
+
+      gmtl::Matrix44f wand_M_obj = wand_M_pobj * curObjPos;
+
+      gmtl::Vec3f obj_dir = gmtl::makeTrans<gmtl::Vec3f>(wand_M_obj);
+     
+      // XXX: Make it configurable if we want to slide the intersection
+      //      point or the center of the object closer to us.
+      obj_dir += mIntersectPoint;
+
+      // XXX: This was removed to allow objects further away to get
+      //      closer faster. If this is normalized again then we will
+      //      need to change the mTransVal >= check to take the length
+      //      of obj_dir into account.
+      //gmtl::normalize(obj_dir);
+
+      // Accumulate translation matrix.
+      gmtl::Matrix44f delta_trans_mat =
+         gmtl::makeTrans<gmtl::Matrix44f>(obj_dir*-mTransValue);
+
+
+      return pobj_M_wand * delta_trans_mat * wand_M_pobj * curObjPos;
    }
-
-   // pobj_M_vp is the inverse of the object in view platform space.
-   OSG::Matrix world_xform;
-   obj.node()->getParent()->getToWorld(world_xform);
-   gmtl::Matrix44f pobj_M_vp;
-   gmtl::set(pobj_M_vp, world_xform);
-   gmtl::invert(pobj_M_vp);
-
-   const gmtl::Matrix44f pobj_M_wand = pobj_M_vp * vp_M_wand;
-   gmtl::Matrix44f wand_M_pobj;
-   gmtl::invert(wand_M_pobj, pobj_M_wand);
-
-   gmtl::Matrix44f wand_M_obj = wand_M_pobj * curObjPos;
-
-   gmtl::Vec3f obj_dir = gmtl::makeTrans<gmtl::Vec3f>(wand_M_obj);
-  
-   // XXX: Make it configurable if we want to slide the intersection
-   //      point or the center of the object closer to us.
-   obj_dir += mIntersectPoint;
-
-   // XXX: This was removed to allow objects further away to get
-   //      closer faster. If this is normalized again then we will
-   //      need to change the mTransVal >= check to take the length
-   //      of obj_dir into account.
-   //gmtl::normalize(obj_dir);
-
-   // Accumulate translation matrix.
-   gmtl::Matrix44f delta_trans_mat =
-      gmtl::makeTrans<gmtl::Matrix44f>(obj_dir*-mTransValue);
-
-
-   return pobj_M_wand * delta_trans_mat * wand_M_pobj * curObjPos;
 }
 
 }
