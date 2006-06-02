b56040d8a5f908a0a6704d5e0d776a57548d98b3
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2006-06-02 22:44:23 +0000

    Added a traverser for IOV scene objects. Bumped version to 0.25.1.

---
+++ IOV/trunk/src/IOV/Util/SceneObjectTraverser.h
@@ -0,0 +1,89 @@
+#ifndef _INF_UTIL_SCENE_OBJECT_TRAVERSER_H_
+#define _INF_UTIL_SCENE_OBJECT_TRAVERSER_H_
+
+#include <IOV/Config.h>
+
+#include <vector>
+#include <boost/function.hpp>
+
+#include <IOV/SceneObjectPtr.h>
+
+
+namespace inf
+{
+
+/**
+ * Basic traverser functions for IOV scene objects.
+ *
+ * @since 0.25.1
+ */
+class IOV_CLASS_API SceneObjectTraverser
+{
+public:
+   /** Traversal results. */
+   enum Result
+   {
+      Continue,         /**< Continue traversing */
+      Skip,             /**< Skip the current subtree of scene objects */
+      Stop              /**< Stop traversing altogether */
+   };
+
+   typedef boost::function<Result (inf::SceneObjectPtr)> enter_func_t;
+   typedef boost::function<Result (inf::SceneObjectPtr, Result)> exit_func_t;
+
+   /** @name Traversal Functions */
+   //@{
+   /**
+    * Traverses the tree of IOV scene objects rooted at \p root using a
+    * depth-first search. During traversal, \p enterFunc is invoked when the
+    * traverser first encounters a scene object, and \p exitFunc is invoked
+    * when exiting the same scene object.
+    *
+    * @param root      The starting point for the traverser.
+    * @param enterFunc The function executed when the traverser enters a new
+    *                  scene object.
+    * @param exitFunc  The function executed when the traverser has finished
+    *                  each scene object subtree. This parameter is optional,
+    *                  and it defaults to a dummy exit function if it is not
+    *                  specified.
+    *
+    * @return The result of executing \p exitFunc on the last exited node is
+    *         returned to the caller.
+    */
+   static Result traverse(inf::SceneObjectPtr root, enter_func_t enterFunc,
+                          exit_func_t exitFunc = defaultExitFunc);
+
+   /**
+    * Traverses the trees of IOV scene objects rooted at \p roots using a
+    * depth-first search on each tree. During traversal, \p enterFunc is
+    * invoked when the traverser first encounters a scene object, and
+    * \p exitFunc is invoked when exiting the same scene object.
+    *
+    * @param roots     The collection of starting points for the traverser.
+    * @param enterFunc The function executed when the traverser enters a new
+    *                  scene object.
+    * @param exitFunc  The function executed when the traverser has finished
+    *                  each scene object subtree. This parameter is optional,
+    *                  and it defaults to a dummy exit function if it is not
+    *                  specified.
+    *
+    * @return The result of executing \p exitFunc on the last exited node is
+    *         returned to the caller.
+    */
+   static Result traverse(const std::vector<inf::SceneObjectPtr>& roots,
+                          enter_func_t enterFunc,
+                          exit_func_t exitFunc = defaultExitFunc);
+   //@}
+
+private:
+   /** Dummy trraversal exit function that simply returns \p r. */
+   static Result defaultExitFunc(inf::SceneObjectPtr, Result r)
+   {
+      return r;
+   }
+};
+
+}
+
+
+#endif /* _INF_UTIL_SCENE_OBJECT_TRAVERSER_H_ */
