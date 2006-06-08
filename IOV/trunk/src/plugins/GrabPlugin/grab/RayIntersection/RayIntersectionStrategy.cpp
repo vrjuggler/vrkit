69d3147b97c934a176e1b5037300bb07ac747bf6
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2006-06-08 22:21:33 +0000

    Use GMTL shell intersection for the ray/volume intersection test instead of OpenSG's volume intersection. This gives the results that we want for this strategy.
    NOTE: GMTL 0.4.11 or newer is now required to build IOV (or at least this
          strategy).

--- IOV/trunk/src/plugins/GrabPlugin/grab/RayIntersection/RayIntersectionStrategy.cpp
+++ IOV/trunk/src/plugins/GrabPlugin/grab/RayIntersection/RayIntersectionStrategy.cpp
@@ -9,6 +9,7 @@
 #include <OpenSG/OSGIntersectAction.h>
 #include <OpenSG/OSGTriangleIterator.h>
 
+#include <gmtl/Intersection.h>
 #include <gmtl/Matrix.h>
 #include <gmtl/External/OpenSGConvert.h>
 
@@ -254,7 +255,6 @@ findIntersection(ViewerPtr viewer, const std::vector<SceneObjectPtr>& objs,
    SceneObjectPtr intersect_obj;
 
    float min_dist = std::numeric_limits<float>::max();   // Set to a max
-   OSG::Line osg_pick_ray;
    OSG::Pnt3f osg_intersect_point;
 
    std::vector<SceneObjectPtr>::const_iterator o;
@@ -280,23 +280,32 @@ findIntersection(ViewerPtr viewer, const std::vector<SceneObjectPtr>& objs,
       const gmtl::Matrix44f obj_M_wand = obj_M_vp * vp_M_wand;
       gmtl::Rayf pick_ray(gmtl::Vec3f(0,0,0), gmtl::Vec3f(0,0,-1));
       gmtl::xform(pick_ray, obj_M_wand, pick_ray);
-      osg_pick_ray.setValue(OSG::Pnt3f(pick_ray.mOrigin.getData()),
-                            OSG::Vec3f(pick_ray.mDir.getData()));
-       
-      float enter_val, exit_val;
+
       OSG::NodeRefPtr root = (*o)->getRoot();
 
-      if ( root->getVolume().intersect(osg_pick_ray, enter_val, exit_val) )
+      OSG::Pnt3f vol_min, vol_max;
+      root->getVolume().getBounds(vol_min, vol_max);
+
+      gmtl::AABoxf bbox(gmtl::Point3f(vol_min[0], vol_min[1], vol_min[2]),
+                        gmtl::Point3f(vol_max[0], vol_max[1], vol_max[2]));
+      unsigned int num_hits;
+      float enter_val, exit_val;
+
+      // Use a GMTL shell intersection test rather than the OpenSG volume
+      // intersection test. Using the shell intersection provides better
+      // results when, for example, the origin of the pick ray is inside a
+      // large volume that contains smaller volumes.
+      if ( gmtl::intersect(bbox, pick_ray, num_hits, enter_val, exit_val) )
       {
-         // If enter_val is 0.0, then the ray is inside of or on the edge of
-         // root's volume. In this case, we only want to consider that for
-         // intersection if no other object has been intersected already.
-         // Otherwise, if enter_val is less than min_dist, then our ray has
-         // intersected an object that is closer than the last intersected
-         // object.
-         if ( enter_val == 0.0f && intersect_obj.get() == NULL ||
-              0.0f < enter_val && enter_val < min_dist )
+         // If enter_val is less than min_dist, then our ray has intersected
+         // an object that is closer than the last intersected object.
+         if ( enter_val < min_dist )
          {
+            const OSG::Line osg_pick_ray(
+               OSG::Pnt3f(pick_ray.mOrigin.getData()),
+               OSG::Vec3f(pick_ray.mDir.getData())
+            );
+
             // If we are doing triangle-level intersection, then we create an
             // intersect action and apply it to the intersected object.
             // Earlier, a callback was registered for handling geometry cores
