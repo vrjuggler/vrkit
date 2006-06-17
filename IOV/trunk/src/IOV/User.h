a561e4722aedc7663ef6f1f83f07135364f0d06c
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2006-06-17 13:52:08 +0000

    Comment updates and improvements. No functional changes.

--- IOV/trunk/src/IOV/User.h
+++ IOV/trunk/src/IOV/User.h
@@ -1,7 +1,7 @@
 // Copyright (C) Infiscape Corporation 2005-2006
 
-#ifndef _INF_USER_H
-#define _INF_USER_H
+#ifndef _INF_USER_H_
+#define _INF_USER_H_
 
 #include <IOV/UserPtr.h>
 #include <IOV/InterfaceTrader.h>
@@ -10,14 +10,18 @@
 
 #include <boost/enable_shared_from_this.hpp>
 
+
 namespace inf
 {
 
 /**
  * This class holds all information related to a user of the application.
- * This includes devices they are using and their position in the virtual world.
+ * This includes devices they are using and their position in the virtual
+ * world.
  *
- * @see Scene for definition of the coordinate frames used.
+ * See inf::Scene for definition of the coordinate frames used.
+ *
+ * @see inf::Scene
  */
 class User : public boost::enable_shared_from_this<User>
 {
@@ -31,8 +35,10 @@ public:
     */
    UserPtr init();
 
-   /** Update user and user associated information.
-    * @post: User and viewplatform are updated.
+   /**
+    * Updates the user and user-associated information.
+    *
+    * @post User and view platform are updated.
     */
    void update(ViewerPtr viewer);
 
@@ -41,7 +47,7 @@ public:
       return mInterfaceTrader;
    }
 
-   /** Return the view platform that we are using. */
+   /** Returns the view platform that we are using. */
    ViewPlatform& getViewPlatform()
    {
       return mViewPlatform;
@@ -55,8 +61,10 @@ private:
    /** Devices abstraction for the user. */
    InterfaceTrader    mInterfaceTrader;
 
-   ViewPlatform   mViewPlatform;    /** The user's view platform. */
+   ViewPlatform   mViewPlatform;    /**< The user's view platform. */
 };
 
 }
+
+
 #endif
--- IOV/trunk/src/IOV/ViewPlatform.h
+++ IOV/trunk/src/IOV/ViewPlatform.h
@@ -1,22 +1,26 @@
 // Copyright (C) Infiscape Corporation 2005-2006
 
-#ifndef _INF_VIEWPLATFORM_H
-#define _INF_VIEWPLATFORM_H
+#ifndef _INF_VIEW_PLATFORM_H_
+#define _INF_VIEW_PLATFORM_H_
 
 #include <IOV/ViewerPtr.h>
 
 #include <gmtl/Matrix.h>
 #include <gmtl/MatrixOps.h>
 
+
 namespace inf
 {
+
 /**
- * This class holds the position of the virtual environment in the virtual world.
- * It is the sort of "magic carpet" that the user is moving around on.
+ * This class holds the position of the virtual environment in the virtual
+ * world. It is the sort of "magic carpet" that the user is moving around on.
  * The view platform corresponds to the physical display system that the
  * user is inside or using.
  *
- * See User for a description of the coordinate systems used.
+ * See inf::Scene for a description of the coordinate systems used.
+ *
+ * @see inf::Scene
  */
 class ViewPlatform
 {
@@ -24,12 +28,12 @@ public:
    ViewPlatform()
    {;}
 
-   /** Update the view platform (navigation) */
+   /** Updates the view platform (navigation). */
    void update(ViewerPtr viewer);
 
-   /** Return the current position.
-    * This is the position of the view platform in the virtual world.
-    * vw_M_vp
+   /**
+    * Returns the current position.
+    * This is the position of the view platform in the virtual world: vw_M_vp.
     */
    const gmtl::Matrix44f& getCurPos() const
    {
@@ -42,8 +46,8 @@ public:
       gmtl::invert(mCurPosInv, mCurPos);
    }
 
-   /** Get the inverse of the current position.
-    * vp_M_vw
+   /**
+    * Returns the inverse of the current position: vp_M_vw.
     */
    const gmtl::Matrix44f& getCurPosInv() const
    {
@@ -51,17 +55,19 @@ public:
    }
 
 private:
-   /* The current positon of the platform in the virtual world. vw_M_vp.
-   * This is the positiong of the virtual platform in the virtual world.
-   */
+   /**
+    * The current positon of the platform in the virtual world: vw_M_vp.
+    * This is the positiong of the virtual platform in the virtual world.
+    */
    gmtl::Matrix44f   mCurPos;
 
-   /** Inverse matrix of the current position.
-    * vp_M_vw
+   /**
+    * Inverse matrix of the current position: vp_M_vw.
     */
    gmtl::Matrix44f   mCurPosInv;
 };
 
 }
+
 
 #endif
