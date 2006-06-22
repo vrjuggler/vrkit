cc78d1d4b2a9534a9a28583acd42fdc882aa3c10
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2006-06-22 21:26:15 +0000

    Added a multi-object grab strategy. Bumped version to 0.30.1.

---
+++ IOV/trunk/src/plugins/GrabPlugin/grab/MultiObjectGrab/MultiObjectGrabStrategy.cpp
@@ -0,0 +1,338 @@
+#include <IOV/Config.h>
+
+#include <functional>
+#include <boost/bind.hpp>
+
+#include <gmtl/External/OpenSGConvert.h>
+
+#include <vpr/Util/Assert.h>
+#include <jccl/Config/ConfigElement.h>
+
+#include <IOV/Plugin/PluginConfig.h>
+#include <IOV/PluginCreator.h>
+#include <IOV/EventData.h>
+#include <IOV/Scene.h>
+#include <IOV/User.h>
+#include <IOV/Viewer.h>
+#include <IOV/InterfaceTrader.h>
+#include <IOV/WandInterface.h>
+#include <IOV/SceneObject.h>
+#include <IOV/StatusPanel.h>
+#include <IOV/StatusPanelPlugin.h>
+
+#include "MultiObjectGrabStrategy.h"
+
+
+static inf::PluginCreator<inf::GrabStrategy> sPluginCreator(
+   &inf::MultiObjectGrabStrategy::create, "Multi-Object Grab Strategy Plug-in"
+);
+
+extern "C"
+{
+
+/** @name Plug-in Entry Points */
+//@{
+IOV_PLUGIN_API(void) getPluginInterfaceVersion(vpr::Uint32& majorVer,
+                                               vpr::Uint32& minorVer)
+{
+   majorVer = INF_GRAB_STRATEGY_PLUGIN_API_MAJOR;
+   minorVer = INF_GRAB_STRATEGY_PLUGIN_API_MINOR;
+}
+
+IOV_PLUGIN_API(inf::PluginCreatorBase*) getGrabStrategyCreator()
+{
+   return &sPluginCreator;
+}
+//@}
+
+}
+
+namespace inf
+{
+
+MultiObjectGrabStrategy::MultiObjectGrabStrategy()
+   : GrabStrategy()
+   , mGrabText("Grab")
+   , mReleaseText("Release")
+   , mGrabbing(false)
+{
+   /* Do nothing. */ ;
+}
+
+MultiObjectGrabStrategy::~MultiObjectGrabStrategy()
+{
+   std::for_each(mIsectConnections.begin(), mIsectConnections.end(),
+                 boost::bind(&boost::signals::connection::disconnect, _1));
+}
+
+GrabStrategyPtr MultiObjectGrabStrategy::init(ViewerPtr viewer)
+{
+   mEventData = viewer->getSceneObj()->getSceneData<EventData>();
+
+   // Connect the intersection signal to our slot.
+   mIsectConnections.push_back(
+      mEventData->mObjectIntersectedSignal.connect(
+         0, boost::bind(&MultiObjectGrabStrategy::objectIntersected, this,
+                        _1, _2, _3, viewer)
+      )
+   );
+
+   // Connect the de-intersection signal to our slot.
+   mIsectConnections.push_back(
+      mEventData->mObjectDeintersectedSignal.connect(
+         0, boost::bind(&MultiObjectGrabStrategy::objectDeintersected,
+                        this, _1)
+      )
+   );
+
+   InterfaceTrader& if_trader = viewer->getUser()->getInterfaceTrader();
+   mWandInterface = if_trader.getWandInterface();
+
+   // Configure
+   std::string elt_type_name = getElementType();
+   jccl::ConfigElementPtr cfg_elt =
+      viewer->getConfiguration().getConfigElement(elt_type_name);
+
+   if ( cfg_elt )
+   {
+      // Configure ourself.
+      configure(cfg_elt);
+   }
+
+   return shared_from_this();
+}
+
+void MultiObjectGrabStrategy::setFocus(ViewerPtr viewer, const bool focused)
+{
+   // If we have focus, we will try to update the staus panel to include our
+   // commands.
+   if ( focused )
+   {
+      inf::ScenePtr scene = viewer->getSceneObj();
+      StatusPanelPluginDataPtr status_panel_data =
+         scene->getSceneData<StatusPanelPluginData>();
+
+      if ( status_panel_data->mStatusPanelPlugin )
+      {
+         // If grab button(s) is/are configured, we will update the status
+         // panel to include that information.
+         if ( mGrabBtn.isConfigured() )
+         {
+            inf::StatusPanel& panel =
+               status_panel_data->mStatusPanelPlugin->getPanel();
+            if ( ! panel.hasControlText(mGrabBtn.getButtons(), mGrabText) )
+            {
+               // The button numbers in mGrabBtn are zero-based, but we would
+               // like them to be one-based in the status panel display.
+               panel.addControlText(transformButtonVec(mGrabBtn.getButtons()),
+                                    mGrabText);
+            }
+         }
+         // If release button(s) is/are configured, we will update the status
+         // panel to include that information.
+         if ( mReleaseBtn.isConfigured() )
+         {
+            inf::StatusPanel& panel =
+               status_panel_data->mStatusPanelPlugin->getPanel();
+            if ( ! panel.hasControlText(mReleaseBtn.getButtons(), mReleaseText) )
+            {
+               // The button numbers in mReleaseBtn are zero-based, but we
+               // would like them to be one-based in the status panel display.
+               panel.addControlText(
+                  transformButtonVec(mReleaseBtn.getButtons()), mReleaseText
+               );
+            }
+         }
+      }
+   }
+   else
+   {
+      inf::ScenePtr scene = viewer->getSceneObj();
+      StatusPanelPluginDataPtr status_panel_data =
+         scene->getSceneData<StatusPanelPluginData>();
+
+      if ( status_panel_data->mStatusPanelPlugin )
+      {
+         inf::StatusPanel& panel =
+            status_panel_data->mStatusPanelPlugin->getPanel();
+
+         // The button numbers in mGrabBtn are zero-based, but we would like
+         // them to be one-based in the status panel display.
+         panel.removeControlText(transformButtonVec(mGrabBtn.getButtons()),
+                                 mGrabText);
+
+         // The button numbers in mReleaseBtn are zero-based, but we would like
+         // them to be one-based in the status panel display.
+         panel.removeControlText(transformButtonVec(mReleaseBtn.getButtons()),
+                                 mReleaseText);
+      }
+   }
+}
+
+void MultiObjectGrabStrategy::update(ViewerPtr viewer,
+                                     grab_callback_t grabCallback,
+                                     release_callback_t releaseCallback)
+{
+   if ( ! mGrabbing )
+   {
+      // The user has requested to add mCurIsectObject to the collection of
+      // objects selected for later grabbing.
+      if ( mCurIsectObject && mCurIsectObject->isGrabbable() &&
+           mChooseBtn.test(mWandInterface, gadget::Digital::TOGGLE_ON) )
+      {
+         mChosenObjects.push_back(mCurIsectObject);
+
+         // Connect the grabbable object state change signal to our slot.
+         mObjConnections[mCurIsectObject] =
+            mCurIsectObject->grabbableStateChanged().connect(
+               boost::bind(&MultiObjectGrabStrategy::grabbableObjStateChanged,
+                           this, _1)
+            );
+
+         // Use the intersection point of the most recently chosen object.
+         // XXX: This isn't exactly right...
+         mIntersectPoint = mCurIntersectPoint;
+      }
+      // The user has requested to grab all the selected objects (those in
+      // mChosenObjects).
+      else if ( ! mChosenObjects.empty() &&
+                mGrabBtn.test(mWandInterface, gadget::Digital::TOGGLE_ON) )
+      {
+         mGrabbing = true;
+         mGrabbedObjects = mChosenObjects;
+         mChosenObjects.clear();
+         grabCallback(mGrabbedObjects, mIntersectPoint);
+      }
+   }
+   // If we are grabbing an object and the release button has just been
+   // pressed, then release the grabbed object.
+   else if ( mGrabbing &&
+             mReleaseBtn.test(mWandInterface, gadget::Digital::TOGGLE_ON) )
+   {
+      // We no longer care about grabbable state changes because
+      // mGrabbedObjects is about to be emptied.
+      std::for_each(
+         mObjConnections.begin(), mObjConnections.end(),
+         boost::bind(&boost::signals::connection::disconnect,
+                     boost::bind(&obj_conn_map_t::value_type::second, _1))
+      );
+
+      // Update our state to reflect that we are no longer grabbing an object.
+      mGrabbing = false;
+      releaseCallback(mGrabbedObjects);
+      mGrabbedObjects.clear();
+   }
+}
+
+void MultiObjectGrabStrategy::configure(jccl::ConfigElementPtr elt)
+{
+   vprASSERT(elt->getID() == getElementType());
+
+   const unsigned int req_cfg_version(1);
+
+   // Check for correct version of plugin configuration.
+   if ( elt->getVersion() < req_cfg_version )
+   {
+      std::stringstream msg;
+      msg << "Configuration of MultiObjectGrabStrategy failed.  Required "
+          << "config element version is " << req_cfg_version
+          << ", but element '" << elt->getName() << "' is version "
+          << elt->getVersion();
+      throw PluginException(msg.str(), IOV_LOCATION);
+   }
+
+   const std::string choose_btn_prop("choose_button_nums");
+   const std::string grab_btn_prop("grab_button_nums");
+   const std::string release_btn_prop("release_button_nums");
+
+   mChooseBtn.configButtons(elt->getProperty<std::string>(choose_btn_prop));
+   mGrabBtn.configButtons(elt->getProperty<std::string>(grab_btn_prop));
+   mReleaseBtn.configButtons(elt->getProperty<std::string>(release_btn_prop));
+}
+
+struct IncValue
+{
+   int operator()(int v)
+   {
+      return v + 1;
+   }
+};
+
+std::vector<int> MultiObjectGrabStrategy::
+transformButtonVec(const std::vector<int>& btns)
+{
+   std::vector<int> result(btns.size());
+   IncValue inc;
+   std::transform(btns.begin(), btns.end(), result.begin(), inc);
+   return result;
+}
+
+inf::Event::ResultType MultiObjectGrabStrategy::
+objectIntersected(SceneObjectPtr obj, SceneObjectPtr parentObj,
+                  gmtl::Point3f pnt, ViewerPtr viewer)
+{
+   if ( ! mGrabbing )
+   {
+      // If we intersected a grabbable object.
+      if ( obj->isGrabbable() )
+      {
+         mCurIsectObject = obj;
+         mCurIntersectPoint = pnt;
+      }
+   }
+   else
+   {
+      return inf::Event::DONE;
+   }
+   
+   return inf::Event::CONTINUE;
+}
+
+inf::Event::ResultType MultiObjectGrabStrategy::
+objectDeintersected(SceneObjectPtr obj)
+{
+   if ( mGrabbing )
+   {
+      return inf::Event::DONE;
+   }
+
+   mCurIsectObject = SceneObjectPtr();
+
+   return inf::Event::CONTINUE;
+}
+
+void MultiObjectGrabStrategy::grabbableObjStateChanged(inf::SceneObjectPtr obj)
+{
+   // If mChosenObjects is not empty, then we have a collection of objects to
+   // grab that have not yet been grabbed. obj may be in mChosenObjects, and
+   // if it is not grabbable, then it must be removed.
+   if ( ! mChosenObjects.empty() )
+   {
+      std::vector<SceneObjectPtr>::iterator o =
+         std::find(mChosenObjects.begin(), mChosenObjects.end(), obj);
+
+      if ( o != mChosenObjects.end() && ! (*o)->isGrabbable() )
+      {
+         mObjConnections[*o].disconnect();
+         mObjConnections.erase(*o);
+         mChosenObjects.erase(o);
+      }
+   }
+   // If mGrabbedObjects is not empty, then have grabbed objects, and obj may
+   // be in that collection. If obj is no longer grabbable, it must be
+   // removed from mGrabbedObjects.
+   else if ( ! mGrabbedObjects.empty() )
+   {
+      std::vector<SceneObjectPtr>::iterator o =
+         std::find(mChosenObjects.begin(), mChosenObjects.end(), obj);
+
+      if ( o != mChosenObjects.end() && ! (*o)->isGrabbable() )
+      {
+         mObjConnections[*o].disconnect();
+         mObjConnections.erase(*o);
+         mGrabbedObjects.erase(o);
+      }
+   }
+}
+
+}
---
+++ IOV/trunk/src/plugins/GrabPlugin/grab/MultiObjectGrab/MultiObjectGrabStrategy.h
@@ -0,0 +1,154 @@
+#ifndef _INF_MULTI_OBJECT_GRAB_STRATEGY_H_
+#define _INF_MULTI_OBJECT_GRAB_STRATEGY_H_
+
+#include <string>
+#include <vector>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/signals/connection.hpp>
+
+#include <gmtl/Matrix.h>
+#include <gmtl/Point.h>
+
+#include <jccl/Config/ConfigElementPtr.h>
+
+#include <IOV/EventDataPtr.h>
+#include <IOV/SceneObjectPtr.h>
+#include <IOV/Grab/GrabStrategy.h>
+#include <IOV/Util/DigitalCommand.h>
+
+
+namespace inf
+{
+
+class MultiObjectGrabStrategy
+   : public GrabStrategy
+   , public boost::enable_shared_from_this<MultiObjectGrabStrategy>
+{
+protected:
+   MultiObjectGrabStrategy();
+
+public:
+   static std::string getId()
+   {
+      return "MultiObjectGrab";
+   }
+
+   static GrabStrategyPtr create()
+   {
+      return GrabStrategyPtr(new MultiObjectGrabStrategy());
+   }
+
+   virtual ~MultiObjectGrabStrategy();
+
+   virtual GrabStrategyPtr init(ViewerPtr viewer);
+
+   virtual void setFocus(ViewerPtr viewer, const bool focused);
+
+   virtual void update(ViewerPtr viewer, grab_callback_t grabCallback,
+                       release_callback_t releaseCallback);
+
+   /**
+    * Invokes the global scope delete operator.  This is required for proper
+    * releasing of memory in DLLs on Windows.
+    */
+   void operator delete(void* p)
+   {
+      ::operator delete(p);
+   }
+
+protected:
+   /**
+    * Deletes this object.  This is an implementation of the pure virtual
+    * inf::GrabStrategy::destroy() method.
+    */
+   virtual void destroy()
+   {
+      delete this;
+   }
+
+private:
+   static std::string getElementType()
+   {
+      return std::string("multi_object_grab_strategy");
+   }
+
+   void configure(jccl::ConfigElementPtr elt);
+
+   std::vector<int> transformButtonVec(const std::vector<int>& btns);
+
+   /**
+    * Responds to object intersection signals.
+    *
+    * @param obj       The object associated with the intersection signal.
+    * @param parentObj The top-most ancestor of \p obj.
+    * @param pnt       The intersection point.
+    * @param viewer    The VR Juggler application object.
+    */
+   inf::Event::ResultType objectIntersected(inf::SceneObjectPtr obj,
+                                            inf::SceneObjectPtr parentObj,
+                                            gmtl::Point3f pnt,
+                                            inf::ViewerPtr viewer);
+
+   /**
+    * Responds to object de-intersection signals.
+    *
+    * @param obj The object associated with the de-intersection signal.
+    */
+   inf::Event::ResultType objectDeintersected(inf::SceneObjectPtr obj);
+
+   /**
+    * Responds to the signal emitted when an object is removed from
+    * inf::GrabData. If \p obj is the object that is currently grabbed, then
+    * the grabbed object is released.
+    *
+    * @param obj The scene object that was removed from inf::GrabData.
+    *
+    * @see releaseGrabbedObject()
+    */
+   void grabbableObjStateChanged(inf::SceneObjectPtr obj);
+
+   EventDataPtr mEventData;
+
+   WandInterfacePtr mWandInterface;
+
+   /** @name Button(s) for adding objects to selection. */
+   //@{
+   inf::DigitalCommand mChooseBtn;
+   std::string mChooseText;
+   //@}
+
+   /** @name Button(s) for grabbing objects. */
+   //@{
+   inf::DigitalCommand mGrabBtn;
+   std::string mGrabText;
+   //@}
+
+   /** @name Button(s) for releasing objects. */
+   //@{
+   inf::DigitalCommand mReleaseBtn;
+   std::string mReleaseText;
+   //@}
+
+   /** @name Intersection State */
+   //@{
+   SceneObjectPtr mCurIsectObject;
+   std::vector<SceneObjectPtr> mChosenObjects;
+   gmtl::Point3f mCurIntersectPoint;
+   gmtl::Point3f mIntersectPoint;
+   std::vector<boost::signals::connection> mIsectConnections;
+   //@}
+
+   /** @name Grab State */
+   //@{
+   bool mGrabbing;
+   std::vector<SceneObjectPtr> mGrabbedObjects;
+   //@}
+
+   typedef std::map<SceneObjectPtr, boost::signals::connection> obj_conn_map_t;
+   obj_conn_map_t mObjConnections;
+};
+
+}
+
+
+#endif /* _INF_MULTI_OBJECT_GRAB_STRATEGY_H_ */
