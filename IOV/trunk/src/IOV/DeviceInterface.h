f3f698b8ab7e43b2353f747fa524eb30aa67560e
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2007-07-17 21:22:08 +0000

    Bring this file back from the dead. I did not notice that inf::DeviceInterface is the base class for inf::WandInterface.

---
+++ IOV/trunk/src/IOV/DeviceInterface.h
@@ -0,0 +1,26 @@
+// Copyright (C) Infiscape Corporation 2005-2007
+
+#ifndef _INF_DEVICEINTERFACE_H
+#define _INF_DEVICEINTERFACE_H
+
+
+namespace inf
+{
+
+class InterfaceTrader;
+
+/**
+* @interface
+*/
+class IOV_CLASS_API DeviceInterface
+{
+private:
+
+   /** @link dependency
+    * @stereotype created by*/
+   /*# inf::InterfaceTrader lnkDeviceInterface; */
+};
+
+}
+
+#endif
