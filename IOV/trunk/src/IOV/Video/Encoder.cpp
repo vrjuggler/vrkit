bbfeee83ffd2b20f401f327ec99bea9dd15d7762
dshipton <dshipton@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2007-08-17 16:48:02 +0000

    Refactor encoder base class.

--- IOV/trunk/src/IOV/Video/Encoder.cpp
+++ IOV/trunk/src/IOV/Video/Encoder.cpp
@@ -16,9 +16,54 @@ Encoder::~Encoder()
    /* Do nothing. */ ;
 }
 
-void Encoder::close()
+Encoder::container_format_list_t Encoder::getSupportedContainersAndCodecs() const
 {
-   /* Do nothing. */ ;
+   return mContainerFormatInfoList;
+}
+
+void Encoder::setEncodingParameters(const encoder_parameters_t params)
+{
+   mEncoderParams = params;
+}
+
+OSG::Image::PixelFormat Encoder::getPixelFormat() const
+{
+   return OSG::Image::OSG_RGB_PF;
+}
+
+std::string Encoder::getName() const
+{
+   return mName;
+}
+
+std::string Encoder::getContainerFormat() const
+{
+   return mEncoderParams.mName;
+}
+
+std::string Encoder::getCodec() const
+{
+   return mEncoderParams.mCodec;
+}
+
+vpr::Uint32 Encoder::getWidth() const
+{
+   return mEncoderParams.mWidth;
+}
+
+vpr::Uint32 Encoder::getHeight() const
+{
+   return mEncoderParams.mHeight;
+}
+
+std::string Encoder::getFilename() const
+{
+   return mEncoderParams.mFilename;
+}
+
+vpr::Uint32 Encoder::getFramesPerSecond() const
+{
+   return mEncoderParams.mFramesPerSecond;
 }
 
 }
--- IOV/trunk/src/IOV/Video/Encoder.h
+++ IOV/trunk/src/IOV/Video/Encoder.h
@@ -10,6 +10,8 @@
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/noncopyable.hpp>
 
+#include <OpenSG/OSGImage.h>
+
 #include <vpr/vprTypes.h>
 #include <vpr/Util/Assert.h>
 
@@ -30,13 +32,56 @@ public:
 
    typedef std::vector<std::string> codec_list_t;
 
-   virtual EncoderPtr init(const std::string& filename, const std::string& encoding,
-                           const vpr::Uint32 width, const vpr::Uint32 height,
-                           const vpr::Uint32 framePerSecond) = 0;
-   virtual void writeFrame(int hidth, int height, vpr::Uint8* data) = 0;
-   virtual vpr::Uint32 width() const = 0;
-   virtual vpr::Uint32 height() const = 0;
-   virtual void close();
+   struct container_format_info_t
+   {
+      std::string		mFormatName;
+      std::string 		mFormatLongName;
+      std::vector<std::string>	mFileExtensions;
+      codec_list_t 		mCodecList;
+      std::string		mEncoderName;
+   };
+
+   struct encoder_parameters_t
+   {
+      std::string	mContainerFormat;
+      std::string	mCodec;
+      std::string	mFilename;
+      vpr::Uint32	mWidth;
+      vpr::Uint32	mHeight;
+      vpr::Uint32	mFramesPerSecond;
+   };
+
+   typedef std::vector<container_format_info_t> container_format_list_t;
+
+   virtual EncoderPtr init() = 0;
+
+   std::string getName() const;
+
+   Encoder::container_format_list_t getSupportedContainersAndCodecs() const;
+
+   void setEncodingParameters(const encoder_parameters_t params);
+
+   virtual void startEncoding() = 0;
+
+   virtual void stopEncoding() = 0;
+
+   virtual void writeFrame(vpr::Uint8* data) = 0;
+
+   virtual OSG::Image::PixelFormat getPixelFormat() const;
+
+protected:
+
+   /* Helper functions into the encoder param struct.*/
+   std::string	getContainerFormat() const;
+   std::string	getCodec() const;
+   vpr::Uint32	getWidth() const;
+   vpr::Uint32	getHeight() const;
+   std::string	getFilename() const;
+   vpr::Uint32	getFramesPerSecond() const;
+
+   std::string			mName;
+   encoder_parameters_t		mEncoderParams;
+   container_format_list_t	mContainerFormatInfoList;
 };
 
 }
