37da7c2a9c93443e9b746f332f81e555df7fecc9
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2007-08-21 19:49:03 +0000

    Wrapped long lines and used our helpers to simplify code.

--- IOV/trunk/src/IOV/Video/DirectShowEncoder.cpp
+++ IOV/trunk/src/IOV/Video/DirectShowEncoder.cpp
@@ -5,6 +5,7 @@
 #include <comdef.h>
 #include <algorithm>
 #include <sstream>
+#include <iterator>
 
 #include <vpr/Util/Assert.h>
 
@@ -83,8 +84,10 @@ DWORD addGraphToRot(IUnknown* graph)
    // to this graph and then GraphEdit exits, this object registration 
    // will be deleted, causing future attempts by GraphEdit to fail until
    // this application is restarted or until the graph is registered again.
-   CHECK_RESULT(rot->Register(ROTFLAGS_REGISTRATIONKEEPSALIVE, graph, moniker, &regId),
-      "Failed to register graph with running object table.");
+   CHECK_RESULT(
+      rot->Register(ROTFLAGS_REGISTRATIONKEEPSALIVE, graph, moniker, &regId),
+      "Failed to register graph with running object table."
+   );
 
    return regId;
 }
@@ -148,7 +151,8 @@ void DirectShowEncoder::startEncoding()
    HRESULT hr;
 
    // Get the interface for DirectShow's GraphBuilder
-   mGraphBuilder = createConcrete(IGraphBuilder, FilterGraph, NULL, CLSCTX_INPROC_SERVER);
+   mGraphBuilder = createConcrete(IGraphBuilder, FilterGraph, NULL,
+                                  CLSCTX_INPROC_SERVER);
 
    // Get the media controller interface from graph builder.
    mMediaController = query(mGraphBuilder, IMediaControl);
@@ -163,33 +167,41 @@ void DirectShowEncoder::startEncoding()
    mByteSource->addPin(mByteStream);
 
    // Add the source to the graph.
-   CHECK_RESULT(mGraphBuilder->AddFilter(mByteSource, L"ByteSource"), "Can't add ByteSource filter");
+   CHECK_RESULT(mGraphBuilder->AddFilter(mByteSource, L"ByteSource"),
+                "Can't add ByteSource filter");
 
    // Create a capture graph builder to help.
    CComPtr<ICaptureGraphBuilder2> capture_graph =
-      createConcrete(ICaptureGraphBuilder2, CaptureGraphBuilder2, NULL, CLSCTX_INPROC_SERVER);
+      createConcrete(ICaptureGraphBuilder2, CaptureGraphBuilder2, NULL,
+                     CLSCTX_INPROC_SERVER);
 
    // Tell the capture graph about the graph builder that we are using.
    capture_graph->SetFiltergraph(mGraphBuilder);
 
    CComPtr<IBaseFilter> avi_mux = NULL;
-   // Use the help of CaptureGraph to build everything we need to write to the given file.
-   hr = capture_graph->SetOutputFileName(&MEDIASUBTYPE_Avi, _bstr_t(getFilename().c_str()), &avi_mux, NULL);
+   // Use the help of CaptureGraph to build everything we need to write to the
+   // given file.
+   hr = capture_graph->SetOutputFileName(&MEDIASUBTYPE_Avi,
+                                         _bstr_t(getFilename().c_str()),
+                                         &avi_mux, NULL);
    setAviOptions(avi_mux, INTERLEAVE_NONE);
 
 #ifdef PREVIEW
    CComPtr<IBaseFilter> smart_tee =
       createConcrete(IBaseFilter, SmartTee, NULL, CLSCTX_INPROC_SERVER);
-   CHECK_RESULT(mGraphBuilder->AddFilter(smart_tee, L"SmartTee"), "Failed to add smart tee!");
+   CHECK_RESULT(mGraphBuilder->AddFilter(smart_tee, L"SmartTee"),
+                "Failed to add smart tee!");
 
    CComPtr<IBaseFilter> video_render =
       createConcrete(IBaseFilter, VideoRenderer, NULL, CLSCTX_INPROC_SERVER);
-   CHECK_RESULT(mGraphBuilder->AddFilter(video_render, L"Preview Window"), "Failed to add renderer!");
+   CHECK_RESULT(mGraphBuilder->AddFilter(video_render, L"Preview Window"),
+                "Failed to add renderer!");
 #endif
 
-   CComPtr<IBaseFilter> encoder = getEncoder("Microsoft MPEG-4  VKI  Codec V3");
-
-   CHECK_RESULT(mGraphBuilder->AddFilter(encoder, L"Encoder"), "Failed to add encoder");
+   CComPtr<IBaseFilter> encoder =
+      getEncoder("Microsoft MPEG-4  VKI  Codec V3");
+   CHECK_RESULT(mGraphBuilder->AddFilter(encoder, L"Encoder"),
+                "Failed to add encoder");
 
    CComPtr<IPin> encoder_in = getInPin(encoder,0);
    CComPtr<IPin> encoder_out = getOutPin(encoder,0);
@@ -203,18 +215,22 @@ void DirectShowEncoder::startEncoding()
    smart_tee->FindPin(L"Preview", &tee_o1);
    smart_tee->FindPin(L"Capture", &tee_o2);
 
-   CHECK_RESULT(mGraphBuilder->Connect(byte_source_out, tee_in), "Failed to connect input to smart tee.");
-   CHECK_RESULT(mGraphBuilder->Connect(tee_o2, encoder_in), "Failed to connect desktop to AviMux!");
+   CHECK_RESULT(mGraphBuilder->Connect(byte_source_out, tee_in),
+                "Failed to connect input to smart tee.");
+   CHECK_RESULT(mGraphBuilder->Connect(tee_o2, encoder_in),
+                "Failed to connect desktop to AviMux!");
 #else
-   CHECK_RESULT(mGraphBuilder->Connect(byte_source_out, encoder_in), "Failed to connect desktop to AviMux!");
+   CHECK_RESULT(mGraphBuilder->Connect(byte_source_out, encoder_in),
+                "Failed to connect desktop to AviMux!");
 #endif
-   CHECK_RESULT(mGraphBuilder->Connect(encoder_out, avimux_in), "Failed to connect desktop to AviMux!");
+   CHECK_RESULT(mGraphBuilder->Connect(encoder_out, avimux_in),
+                "Failed to connect desktop to AviMux!");
 
 #ifdef PREVIEW
    mGraphBuilder->Render(tee_o1);
 #endif
 
-   CHECK_RESULT(hr = mMediaController->Run(), "Failed to run DirectShow Error: ");
+   CHECK_RESULT(mMediaController->Run(), "Failed to run DirectShow Error!");
 
 #ifdef REGISTER_GRAPH
    mGraphRegister = addGraphToRot(mGraphBuilder);
@@ -223,7 +239,8 @@ void DirectShowEncoder::startEncoding()
 
 void DirectShowEncoder::stopEncoding()
 {
-   CHECK_RESULT(mMediaController->StopWhenReady(), "Failed to stop direct show.");
+   CHECK_RESULT(mMediaController->StopWhenReady(),
+                "Failed to stop direct show.");
 
 #ifdef REGISTER_GRAPH
    if (mGraphRegister)
@@ -251,38 +268,27 @@ void DirectShowEncoder::writeFrame(vpr::Uint8* data)
 
 #define PREVIEW
 
-void DirectShowEncoder::setAviOptions(IBaseFilter *ppf, InterleavingMode INTERLEAVE_MODE)
+void DirectShowEncoder::setAviOptions(IBaseFilter* ppf,
+                                      InterleavingMode INTERLEAVE_MODE)
 {
-   CComPtr<IConfigAviMux>        avi_mux           = NULL;
-   CComPtr<IConfigInterleaving>  interleaving  = NULL;
-
    vprASSERT(ppf);
    if (!ppf)
    {
       throw inf::Exception("Can't have NULL AVI Mux.", IOV_LOCATION);
    }
-   // QI for interface AVI Muxer
-   if (FAILED(ppf->QueryInterface(IID_IConfigAviMux, reinterpret_cast<PVOID *>(&avi_mux))))
-   {
-      throw inf::Exception("CDVGraph::SetAviOptions::QI IConfigAviMux failed.", IOV_LOCATION);
-   }
 
-   if (FAILED(avi_mux->SetOutputCompatibilityIndex(TRUE)))
-   {
-      throw inf::Exception("CDVGraph::SetAviOptions::avi_mux->SetOutputCompatibilityIndex failed.", IOV_LOCATION);
-   }
+   // QI for interface AVI Muxer
+   CComPtr<IConfigAviMux> avi_mux = query(CComPtr<IBaseFilter>(ppf),
+                                          IConfigAviMux);
+   CHECK_RESULT(avi_mux->SetOutputCompatibilityIndex(TRUE),
+                "avi_mux->SetOutputCompatibilityIndex failed.");
 
    // QI for interface Interleaving
-   if (FAILED(ppf->QueryInterface(IID_IConfigInterleaving, reinterpret_cast<PVOID *>(&interleaving))))
-   {
-      throw inf::Exception("CDVGraph::SetAviOptions::QI IConfigInterleaving failed.", IOV_LOCATION);
-   }
-
+   CComPtr<IConfigInterleaving> interleaving =
+      query(CComPtr<IBaseFilter>(ppf), IConfigInterleaving);
    // put the interleaving mode (full, none, half)
-   if (FAILED(interleaving->put_Mode(INTERLEAVE_MODE)))
-   {
-      throw inf::Exception("CDVGraph::SetAviOptions::interleaving->put_Mode failed.", IOV_LOCATION);
-   }
+   CHECK_RESULT(interleaving->put_Mode(INTERLEAVE_MODE),
+                "interleaving->put_Mode failed.");
 } 
 
 DirectShowEncoder::moniker_list_t DirectShowEncoder::getVideoMonikers()
@@ -293,7 +299,8 @@ DirectShowEncoder::moniker_list_t DirectShowEncoder::getVideoMonikers()
    CComPtr<IMoniker> pMoniker = NULL;
 
    CComPtr<ICreateDevEnum> sys_dev_enum =
-      createConcrete(ICreateDevEnum, SystemDeviceEnum, NULL, CLSCTX_INPROC_SERVER);
+      createConcrete(ICreateDevEnum, SystemDeviceEnum, NULL,
+                     CLSCTX_INPROC_SERVER);
 
    // S_FALSE means nothing in this category.
    if (S_OK == sys_dev_enum->CreateClassEnumerator(CLSID_VideoCompressorCategory, &enum_moniker, 0))
@@ -315,10 +322,12 @@ DirectShowEncoder::codec_list_t DirectShowEncoder::getCodecs()
    codec_list_t encoder_names;
 
    // Iterate over monikers trying to grab the friendly name.
-   for (moniker_list_t::iterator m = monikers.begin(); m != monikers.end(); ++m)
+   typedef moniker_list_t::iterator iter_type;
+   for ( iter_type m = monikers.begin(); m != monikers.end(); ++m )
    {
       CComPtr<IPropertyBag> pPropBag = NULL;
-      (*m)->BindToStorage(0, 0, IID_IPropertyBag, (void **)&pPropBag);
+      (*m)->BindToStorage(0, 0, IID_IPropertyBag,
+                          reinterpret_cast<void**>(&pPropBag));
       VARIANT var;
       VariantInit(&var);
       if (SUCCEEDED(pPropBag->Read(L"FriendlyName", &var, 0)))
@@ -333,13 +342,10 @@ DirectShowEncoder::codec_list_t DirectShowEncoder::getCodecs()
 
 void DirectShowEncoder::printVideoEncoders()
 {
-   std::vector<std::string> encoder_names;
-   encoder_names = getCodecs();
-   for (std::vector<std::string>::iterator itr = encoder_names.begin();
-        itr != encoder_names.end(); ++itr)
-   {
-      std::cout << *itr << std::endl;
-   }
+   const std::vector<std::string> encoder_names = getCodecs();
+   std::copy(encoder_names.begin(), encoder_names.end(),
+             std::ostream_iterator<std::string>(std::cout, "\n"));
+   std::cout << std::flush;
 }
 
 void DirectShowEncoder::printPins(CComPtr<IBaseFilter> filter)
@@ -365,10 +371,12 @@ CComPtr<IBaseFilter> DirectShowEncoder::getEncoder(const std::string name)
    HRESULT hr;
    moniker_list_t monikers = getVideoMonikers();
 
-   for (moniker_list_t::iterator m = monikers.begin(); m != monikers.end(); ++m)
+   typedef moniker_list_t::iterator iter_type;
+   for ( iter_type m = monikers.begin(); m != monikers.end(); ++m )
    {
       CComPtr<IPropertyBag> pPropBag = NULL;
-      (*m)->BindToStorage(0, 0, IID_IPropertyBag, (void **)&pPropBag);
+      (*m)->BindToStorage(0, 0, IID_IPropertyBag,
+                          reinterpret_cast<void**>(&pPropBag));
       VARIANT var;
       VariantInit(&var);
       hr = pPropBag->Read(L"FriendlyName", &var, 0);
@@ -378,7 +386,9 @@ CComPtr<IBaseFilter> DirectShowEncoder::getEncoder(const std::string name)
          if (enc_name == name)
          {
             CComPtr<IBaseFilter> filter = NULL;
-            hr = (*m)->BindToObject(NULL, NULL, IID_IBaseFilter, (void**)&filter);
+            hr = (*m)->BindToObject(NULL, NULL, IID_IBaseFilter,
+                                    reinterpret_cast<void**>(&filter));
+
             if (SUCCEEDED(hr))
             {
                return filter;
