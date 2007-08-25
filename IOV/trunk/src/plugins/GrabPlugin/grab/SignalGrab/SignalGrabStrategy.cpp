6853a022d077aa8168f4d5a27f5ccde3bf7c8a58
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2007-08-25 15:56:39 +0000

    Switch to the LGPL license, which includes the official declaration that this library is called vrkit.

--- IOV/trunk/src/plugins/GrabPlugin/grab/SignalGrab/SignalGrabStrategy.cpp
+++ IOV/trunk/src/plugins/GrabPlugin/grab/SignalGrab/SignalGrabStrategy.cpp
@@ -1,4 +1,20 @@
-// Copyright (C) Infiscape Corporation 2005-2007
+// vrkit is (C) Copyright 2005-2007
+//    by Allen Bierbaum, Aron Bierbuam, Patrick Hartling, and Daniel Shipton
+//
+// This file is part of vrkit.
+//
+// vrkit is free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version.
+//
+// vrkit is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <IOV/Config.h>
 
