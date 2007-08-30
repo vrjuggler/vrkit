d8795cc63afdcb23a15d6a93bb83bfe533257f0b
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2007-08-30 15:02:51 +0000

    Whoops, these were supposed to have been added with r1283.

---
+++ IOV/trunk/src/vrkit/scenedata/MaterialPoolData.cpp
@@ -0,0 +1,49 @@
+// vrkit is (C) Copyright 2005-2007
+//    by Allen Bierbaum, Aron Bierbuam, Patrick Hartling, and Daniel Shipton
+//
+// This file is part of vrkit.
+//
+// vrkit is free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version.
+//
+// vrkit is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <vrkit/scenedata/MaterialPoolData.h>
+
+
+namespace vrkit
+{
+
+const vpr::GUID
+MaterialPoolData::type_guid("3f933047-ef3d-44fb-9553-d6262fcc000b");
+
+MaterialPoolData::MaterialPoolData()
+   : mMaterialPool(OSG::NullFC)
+{
+   /* Do nothing. */ ;
+}
+
+MaterialPoolData::~MaterialPoolData()
+{
+   /* Do nothing. */ ;
+}
+
+OSG::MaterialPoolRefPtr MaterialPoolData::getMaterialPool()
+{
+   if ( OSG::NullFC == mMaterialPool )
+   {
+      mMaterialPool = OSG::MaterialPool::create();
+   }
+
+   return mMaterialPool;
+}
+
+}
