fc13c66c5cdc61720d49ca1ea1ff139bb879effa
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2007-08-30 20:02:34 +0000

    Removed branches and tags that are no longer of value.

--- IOV/branches/0.15/data/shaders/highlight.fs
+++
@@ -1,8 +0,0 @@
-varying float alpha;
-uniform vec3 color;
-
-
-void main(void)
-{
-    gl_FragColor = vec4(color, alpha);
-}
--- IOV/tags/release-0.2.0/data/shaders/highlight.fs
+++
@@ -1,7 +0,0 @@
-varying float alpha;
-
-
-void main(void)
-{
-    gl_FragColor = vec4(1.0, 1.0, 0.0, alpha);
-}
