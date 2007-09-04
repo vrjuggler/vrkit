5dab2af83f341f8bf30ebb17beaf12ab20829e69
patrick <patrick@5b7fcf1c-1fbb-11df-8982-67e453f37615>
2007-09-04 22:56:05 +0000

    Noted Dan's work on versioned installation and automatic environment variable assignment. Bumped the version to 0.49.0.

--- trunk/src/vrkit/Version.h
+++ trunk/src/vrkit/Version.h
@@ -34,8 +34,8 @@
  */
 // The major/minor/patch version (up to 3 digits each).
 #define VRKIT_VERSION_MAJOR    0
-#define VRKIT_VERSION_MINOR    48
-#define VRKIT_VERSION_PATCH    3
+#define VRKIT_VERSION_MINOR    49
+#define VRKIT_VERSION_PATCH    0
 
 //--------------------------------------------------------------------------
 //--------------------------------------------------------------------------
